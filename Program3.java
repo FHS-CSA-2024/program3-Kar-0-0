e1a5e2ca06558ae2040a594669dc67c082b043e7
Kartik Mehta <76859229+Kar-0-0@users.noreply.github.com>
2024-08-13 09:37:29 -0700

    Update Program3.java
    Used user interface to calculate the area and perimeter of the rectangle using the users width and length.

--- Program3.java
+++ Program3.java
@@ -3,12 +3,41 @@
 import java.util.Scanner; //This imports the console scanner that reads user input
 
 //Your code here:
+public class Program3 {
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);
+        int length = 0;
+        int width = 0;
+        
+        System.out.println("Enter the length: ");
+        length = myObj.nextInt();
+        
+        System.out.println("Enter the width: ");
+        width = myObj.nextInt();
+        
+        int area = length * width;
+        int perimeter = (2 * length) + (2 * width);
+        
+        
+        System.out.println("This is the area: " + area + "\n" 
+        + "This is the periemter: " + perimeter);
+        
+        
+        
+        
+    }
+}
 
 
 
 
 //Paste console output below:
 /*
-
+Enter the length: 
+143
+Enter the width: 
+82
+This is the area: 11726
+This is the periemter: 450
 
 */
